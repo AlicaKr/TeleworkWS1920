6d418b1c5267765341e6c1480544f2dbfb948b49
katjakvr <56429435+katjakvr@users.noreply.github.com>
2019-11-05 20:37:25 +0100

    AnalysisScript angelegt

---
+++ AnalysisScript.R
@@ -0,0 +1,10 @@
+# AnalysisScript.R
+## Teamname: Telework
+
+# Pakete laden: ----
+
+# Rohdaten laden: ----
+
+# DataCleaning und Skalenberechnung: ----
+
+# Statistische Analyse und Grafiken: ----
